a7abd83e38314cfecdd7610ac300ea5e563c4d48
Александр <medvedev.sasha2000@mail.ru>
2020-11-03 20:43:42 +0300

    Remove travel time property

--- src/main/java/com/railwayApp/api/dto/RouteDto.java
+++ src/main/java/com/railwayApp/api/dto/RouteDto.java
@@ -21,28 +21,6 @@ public class RouteDto {
     private Station toUserStation;
     private Integer departureTime;
     private Integer arrivalTime;
-    private Integer travelTime;
-
-
-    public RouteDto(Route route) {
-        this.id = route.getId();
-        this.train = route.getTrain();
-
-        List<Station> routeStations = route.getStations();
-        Station fromStation = routeStations.get(0);
-        Station toStation = routeStations.get(routeStations.size() - 1);
-        this.fromRouteStation = fromStation;
-        this.toRouteStation = toStation;
-        this.fromUserStation = fromStation;
-        this.toUserStation = toStation;
-
-        List<Schedule> routeSchedule = route.getSchedule();
-        Schedule departureSchedule = routeSchedule.get(0);
-        Schedule arrivalSchedule = routeSchedule.get(routeSchedule.size() - 1);
-        this.departureTime = departureSchedule.getDepartureTime();
-        this.arrivalTime = arrivalSchedule.getArrivalTime();
-        this.travelTime = arrivalTime - departureTime;
-    }
 
     public RouteDto(Route route, Station fromStation, Station toStation) {
         this.id = route.getId();
@@ -61,7 +39,6 @@ public class RouteDto {
         Schedule arrivalSchedule = routeSchedule.get(toStationIndex);
         this.departureTime = departureSchedule.getDepartureTime();
         this.arrivalTime = arrivalSchedule.getArrivalTime();
-        this.travelTime = arrivalTime - departureTime;
     }
 
 }
